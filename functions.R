0f58ae2a4b89fa0697e96ac5de6fa5f1bbeb6592
N.A.C <allenansa@gmail.com>
2018-11-04 10:41:39 -0600

    Update functions.R

--- functions.R
+++ functions.R
@@ -1,22 +1,24 @@
 library(sp)
-d = list(x=c(1,2,3),y=c(1,2,3),z=c(1,2,3))
 
 find_Neigh <- function(d,h){
   N = length(d$x)
-  d_matrix <- dist(cbind(d$x,d$y),diag = 1, upper = 1)
+  d_matrix <- as.matrix(dist(cbind(d$x,d$y),diag = 1, upper = 1))
   d$id <- matrix(rep(0,N*N),N,N)
   d$id[d_matrix<h]=1
   return(d)
 }
+d<-find_Neigh(d,80000)
+corrplot(d$id)
 
 Raw_est <- function(d){
   n=length(d$x)
   for( i in c(1:n)){
-    t <- data.frame(x=d$x[d$id[i,]],y=d$y[d$id[i,]],z=x=d$z[d$id[i,]])
+    t <- data.frame(x=d$x[which(d$id[i,]==1)],y=d$y[which(d$id[i,]==1)],z=d$z[which(d$id[i,]==1)])
     coordinates(t) = ~x+y
     vgm <- variogram(z~1,t)
     est <- fit.variogram(vgm, vgm("Mat"))
-    d$est <- rbind(d$est,c(est$psill,est$range,est$kappa))
+    d$est <- rbind(d$est,c(est$psill[2],est$range[2],est$kappa[2]))
   }
   return(d)
 }
+d<-Raw_est(d_list)
